7c6ec839d24b108e32877e337e0f4a40d956d113
syedadil21 <syed_adil21@live.com>
2026-04-09 19:15:38 +0500

    Remove unused Certifications component import from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Education from "./components/Education";
-import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import LoadingScreen from "./components/LoadingScreen";
